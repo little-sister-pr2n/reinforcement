f30d487cc04cbd039825d81a777e6584d3a6f938
little-sister-pr2n <rinzemuko@gmail.com>
2022-04-07 07:54:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--time", default=1, type=int)
 args = parser.parse_args()
 for t in range(args.time):
-    sleep(t)
+    sleep(1)
     print(t)
