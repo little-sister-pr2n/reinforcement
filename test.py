461e6670ceeb3f2af372cc414ec95ead6d821ba4
little-sister-pr2n <rinzemuko@gmail.com>
2022-04-07 08:00:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import argparse
+import os 
 from time import sleep
 parser = argparse.ArgumentParser()
 parser.add_argument("--time", default=1, type=int)
@@ -6,3 +7,6 @@ args = parser.parse_args()
 for t in range(args.time):
     sleep(1)
     print(t)
+    with open(f"testing{t}.txt", "a") as f:
+        f.write("")
+    
